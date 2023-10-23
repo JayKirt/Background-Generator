3b2c8e01f9a48a3bb09f98e4041f2b45cc6b4f80
Jay Kirtlan <jaykirtlan@hotmail.com>
2023-10-23 13:41:37 +0100

    using add event listener with an input argument to console log the value of each color within the HTML input field

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ var color2 = document.querySelector (".color2");
 console.log(css);
 console.log(color1);
 console.log(color2);
+
+color1.addEventListener("input", function (){
+    console.log(color1.value)
+}
+)
+
+color2.addEventListener("input", function (){
+    console.log(color2.value)
+}
+)
